964c984c0c2c888050f57fec4ee524fc9799aec0
Richard Oh <richard@magic.link>
2022-08-15 12:30:11 -0500

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,37 @@
+import { useState } from 'react';
+import { Magic } from 'magic-sdk';
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const [emailAddress, setEmailAddress] = useState('')
+  const [storedValue, setStoredValue] = useState('')
+
+  const handleEmailChange = (e) => {
+    setEmailAddress(e.target.value);
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const magic = await new Magic(
+      process.env.NEXT_PUBLIC_MAGIC_PUBLISHABLE_KEY
+    )
+    magic.auth.loginWithMagicLink({
+      email: emailAddress
+    })
+    setEmailAddress('')
+  };
+
+  const handleValueChange = (e) => {
+    setStoredValue(e.target.value);
+  };
+
+  const handleStoreString = (e) => {
+    e.preventDefault();
+    localStorage.setItem('string', storedValue);
+    setStoredValue('');
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,57 +42,39 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Next PWA Demo
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Store string value and/or store valid Magic session to local storage.
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <div className={styles.card}>
+            <h2>Store {!storedValue ? 'String' : storedValue} &rarr;</h2>
+            <p>Store this string to local storage</p>
+            <input
+              style={{marginTop: '1rem'}}
+              onChange={handleValueChange}
+              type='text'
+              value={storedValue}
+            />
+            <button onClick={handleStoreString}>Store</button>
+          </div>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <div className={styles.card}>
+            <h2>Store Magic &rarr;</h2>
+            <p>Store Magic session to local storage</p>
+            <input
+              style={{marginTop: '1rem'}}
+              onChange={handleEmailChange}
+              type='text'
+              value={emailAddress}
+              />
+            <button onClick={handleLogin}>Login</button>
+          </div>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
